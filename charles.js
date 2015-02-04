877afaa39e1e8bf26aeae58c5fc9d8d98023bbdc
Charley Robinson <charley@tokbox.com>
2015-02-04 12:24:34 -0700

    Updated styles

--- charles.js
+++ charles.js
@@ -5,15 +5,17 @@ function charles() {
     tmptxt,
     tmpnode,
     // compile the RE and cache the replace string, for performance
-    cakeRE = /Jenkins/g
+    charlesRE = /Jenkins/g
     replaceValue = "Charles";
 
     // iterate through all text nodes
     while (allTextNodes.nextNode()) {
         tmpnode = allTextNodes.currentNode;
         tmptxt = tmpnode.nodeValue;
-        tmpnode.nodeValue = tmptxt.replace(cakeRE,replaceValue);
+        tmpnode.nodeValue = tmptxt.replace(charlesRE,replaceValue);
     }
+    
+    document.title = document.title.replace(charlesRE,replaceValue);
 }
 
 window.onload = charles;
